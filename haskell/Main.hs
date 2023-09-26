d8793fe7afdc5cff1a90aeec4f15c91e2f6031a9
KnightLeo <80933008+Knightleo602@users.noreply.github.com>
2023-09-26 18:41:02 -0300

    Update Main.hs

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,8 +1,12 @@
 module Main where
 
-fib x y n
-  | n == 1 = y
-  | otherwise = fib y (x+y) (n-1)
+-- essa versao e muito mais parecida com a versao feita em python
+-- fib x y n
+--   | n == 1 = y
+--   | otherwise = fib y (x+y) (n-1)
+
+fib x y 1 = y
+fib x y n = fib y (x+y) (n-1)
 
 main = do
   print ("Fibonacci " ++ show (fib 0 1 50))
